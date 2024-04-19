e3330c5aabd4f076ac3c6f464c6c947eb5257df7
KOT-MEOW <QAZRFV123a>
2024-04-19 17:43:29 +0300

    Authors

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use Illuminate\View\View;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', [HomeController::class, 'index']);
 
+Route::get('/authors', [AuthorController::class, 'list']);
+Route::get('/authors/create', [AuthorController::class, 'create']);
+Route::post('/authors/put', [AuthorController::class, 'put']);
