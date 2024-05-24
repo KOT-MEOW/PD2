2db3175dd7486bf56eb88c1d0edb34c268343295
KOT-MEOW <QAZRFV123a>
2024-05-24 16:58:31 +0300

    24.05.2024 nodarbibas kods

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DataController;
 
 // Auth routes
 Route::get('/login', [AuthController::class, 'login'])->name('login');
@@ -38,3 +39,7 @@ Route::get('/categories/update/{category}', [CategoryController::class, 'update'
 Route::post('/categories/patch/{category}', [CategoryController::class, 'patch']);
 Route::post('/categories/delete/{category}', [CategoryController::class, 'delete']);
 
+// Data/API
+Route::get('/data/get-top-books', [DataController::class, 'getTopBooks']);
+Route::get('/data/get-book/{book}', [DataController::class, 'getBook']);
+Route::get('/data/get-related-books/{book}', [DataController::class, 'getRelatedBooks']);
